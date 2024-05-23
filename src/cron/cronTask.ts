29f262c4838476bf19c58fb3e56f23ade65d3832
VuViet <vuhoangviet@cryptopie-labo.com>
2024-05-23 21:16:43 +0700

    add trycatch

--- src/cron/cronTask.ts
+++ src/cron/cronTask.ts
@@ -12,36 +12,40 @@ const job = new CronJob("*/10 * * * * *", () => {
 const checkReleasePoolToken = new CronJob("*/10 * * * * *", async () => {
   const contract = "dd.tg";
   console.log(`running cron job crawl pool token ${contract}...`);
-  const raw = await axios.get(`https://api.ref.finance/list-pools`, {});
+  try {
+    const raw = await axios.get(`https://api.ref.finance/list-pools`, {});
 
-  const filterToken = raw?.data?.filter(
-    (i: any) =>
-      bigNumber(i?.tvl).gt(0) &&
-      bigNumber(i?.tvl).gt(0) &&
-      (i?.token_account_ids as Array<string>).includes(contract) &&
-      ((i?.token_account_ids as Array<string>).includes("wrap.near") ||
-        (i?.token_account_ids as Array<string>).includes(
-          "usdt.tether-token.near"
-        ))
-  );
+    const filterToken = raw?.data?.filter(
+      (i: any) =>
+        bigNumber(i?.tvl).gt(0) &&
+        bigNumber(i?.tvl).gt(0) &&
+        (i?.token_account_ids as Array<string>).includes(contract) &&
+        ((i?.token_account_ids as Array<string>).includes("wrap.near") ||
+          (i?.token_account_ids as Array<string>).includes(
+            "usdt.tether-token.near"
+          ))
+    );
 
-  const rsFocus: Array<any> = filterToken
-    .sort((a: any, b: any) => (bigNumber(a.tvl).gte(b.tvl) ? -1 : 1))
-    .map((i: any) => {
-      return {
-        // ...i,
-        id: i?.id,
-        token_account_ids: i?.token_account_ids,
-        token_symbols: i?.token_symbols,
-        token_price: i?.token0_ref_price,
-        liq: formatBalance(i?.tvl),
-      };
-    });
+    const rsFocus: Array<any> = filterToken
+      .sort((a: any, b: any) => (bigNumber(a.tvl).gte(b.tvl) ? -1 : 1))
+      .map((i: any) => {
+        return {
+          // ...i,
+          id: i?.id,
+          token_account_ids: i?.token_account_ids,
+          token_symbols: i?.token_symbols,
+          token_price: i?.token0_ref_price,
+          liq: formatBalance(i?.tvl),
+        };
+      });
 
-  if (rsFocus.length) {
-    handlePushTelegramNotification({
-      body: rsFocus.map((i: any) => generateTelegramHTML(i)).join("\n\n"),
-    });
+    if (rsFocus.length) {
+      handlePushTelegramNotification({
+        body: rsFocus.map((i: any) => generateTelegramHTML(i)).join("\n\n"),
+      });
+    }
+  } catch (error) {
+    console.log(`error`);
   }
 
   return;
