a0311b2f416225beaa633f3d9a654cb9ada62845
VuViet <vuhoangviet@cryptopie-labo.com>
2024-09-30 09:55:47 +0700

    update

--- src/seeds/price-token.seed.ts
+++ src/seeds/price-token.seed.ts
@@ -1,4 +1,9 @@
 export const listPriceSeed: Record<string, any> = {
+  "gp.token0.near": {
+    price: "7.5",
+    decimal: 18,
+    symbol: "KAT",
+  },
   "kat.token0.near": {
     price: "7.5",
     decimal: 18,
