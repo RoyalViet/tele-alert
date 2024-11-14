44287539077b2928f6b1fd1f3a1c3ba0e8d1f8a1
Royal <royalvietvu@gmail.com>
2024-11-14 16:16:02 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ const main = async () => {
   // job.start();
   checkMemeCooking.start();
   checkRefPoolToken.start();
-  checkRadiumPoolToken.start();
+  // checkRadiumPoolToken.start();
   // checkReleasePoolToken.start();
 
   // test
