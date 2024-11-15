5431cadd3ad35a835daf3a07862d2be60a15d832
ROYAL\vuhoa <vuhoangviet160799@gmail.com>
2024-11-15 18:40:53 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ const main = async () => {
   // TODO: run
   checkMemeCooking.start();
   checkRefPoolToken.start();
-  checkRadiumPoolToken.start();
+  // checkRadiumPoolToken.start();
 
   // checkReleasePoolToken.start();
 
